8ed370ab3a17c974dec9b651357755b6adc4515b
〲FBニꙮ 〲ཽ࿆KINGཽ࿆ 豐࿐ <mxjowel330@gmail.com>
2025-04-18 13:25:20 -0700

    Update ｢File｣__autoreact.js

--- Script/commands/｢File｣__autoreact.js
+++ Script/commands/｢File｣__autoreact.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
  let haha = event.body.toLowerCase();
   if (haha.includes("☘️") || haha.includes("😂") || haha.includes("🥰") || haha.includes("🌺") || haha.includes("🌸") || haha.includes("😌") || haha.includes("🐰") || haha.includes("🍒") || haha.includes("😊") || haha.includes("💜") || 
 haha.includes("❤️") || haha.includes("✨") || haha.includes("😎") || haha.includes("😑") || haha.includes("😇")){                 
-    return api.setMessageReaction("🌺", event.messageID, (err) => {}, true)
+    return api.setMessageReaction("🤖", event.messageID, (err) => {}, true)
     api.markAsSeen(1, (err) => {});
   }
     if (haha.includes("🫡") || haha.includes("🤐") || haha.includes("😏") || haha.includes("☹️") || haha.includes("🤧") || haha.includes("🙄") || haha.includes("🤗") || haha.includes("😢") || haha.includes("😋") || haha.includes("🥴") || haha.includes("😎")){
@@ -24,19 +24,19 @@ haha.includes("❤️") || haha.includes("✨") || haha.includes("😎") || haha
     return api.setMessageReaction("🌸", event.messageID, (err) => {}, true)
   }
   if (haha.includes("i love you") || haha.includes("love you")){
-    api.sendMessage("আমাকে নাহ আল্লাহ কে ভালোবাসা প্রয় ভাই ও বন 🌺✨", event.threadID, event.messageID)
+    api.sendMessage("আমাকে নাহ আল্লাহ কে ভালোবাস 🌺✨", event.threadID, event.messageID)
   }
   if (haha.includes("ummah") || haha.includes("kiss me bot")){
     api.sendMessage("আমি রোবোট আমার চুম্মা খেলে কি তুমার ফিলিংস আসবে আচ্ছা বাবা নাও খেয়ে দেখো মজা পাও কি নাহ\n\n\n 👉🤖👈\n\n ummmmah 💋💋🥰365 Day", event.threadID, event.messageID)
   }
-  if (haha.includes("love me bot") || haha.includes("love me jan")){
+  if (haha.includes("love me bot") || haha.includes("...")){
     api.sendMessage("ok Baby love too❤️💋", event.threadID, event.messageID)
   }
 if (haha.includes("biye korbe bot") || haha.includes("বিয়ে করবা বট")){
-    api.sendMessage("আমার তো ওইটা নেই আমাকে বিয়ে করে কি করব 😐", event.threadID, event.messageID)
+    api.sendMessage("আমার তো ওইটা নেই আমাকে বিয়ে করে কি করবে 😐", event.threadID, event.messageID)
 }
   if (haha.includes("miss you bot") || haha.includes("i miss you bot")){
-    api.sendMessage("miss you to 😌🥹", event.threadID, event.messageID)
+    api.sendMessage("miss you too 😌🥹", event.threadID, event.messageID)
       }
 }
         module.exports.run = function({ api, event, client, __GLOBAL }) {
