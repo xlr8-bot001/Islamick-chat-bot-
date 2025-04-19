cb34b481c95b7afbac344f714397efb5777449a5
〲FBニꙮ 〲ཽ࿆KINGཽ࿆ 豐࿐ <mxjowel330@gmail.com>
2025-04-19 12:39:01 -0700

    Update slap.js

--- Script/commands/slap.js
+++ Script/commands/slap.js
@@ -14,10 +14,10 @@ module.exports.config = {
 };
 
 module.exports.run = async({ api, event, Threads, global }) => {
-  var link = [ "https://i.postimg.cc/FRkmyJGg/received-399842312524108.gif", "https://i.postimg.cc/BndjPDTs/received-341085165515891.gif" , "https://i.postimg.cc/bw65rCzc/received-2735245696641329.gif" , "https://i.postimg.cc/Xq88hqqS/received-351791154294741.gif" , "https://i.postimg.cc/0yGwsnCJ/received-291977343887514.gif" ];
+  var link = [ "https://i.postimg.cc/FRkmyJGg/received-100089438197003.gif", "https://i.postimg.cc/BndjPDTs/received-100092166793458.gif" , "https://i.postimg.cc/bw65rCzc/received-100089438197003.gif" , "https://i.postimg.cc/Xq88hqqS/received-100089438197003.gif" , "https://i.postimg.cc/0yGwsnCJ/received-100092166793458.gif" ];
    var mention = Object.keys(event.mentions);
      let tag = event.mentions[mention].replace("@", "");
     if (!mention) return api.sendMessage("Mention 1 person that you want to slap", threadID, messageID);
-   var callback = () => api.sendMessage({body:`╭──────•◈•───────╮\n         𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁       \n\n_থাপ্পর👋 খা হালার পুত_😛👉 ${tag}` + `\n\nসরি, আমার ভুল হয়েছে মাফ করে দিন_🙁😸\n\n  BOT OWNER Jowel ッ\n╰──────•◈•───────╯`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/slp.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/slp.gif"));  
+   var callback = () => api.sendMessage({body:`╭──────•◈•───────╮\n         𝗫𝗟𝗥𝟖  𝗖𝗛𝗔𝗧 - 𝗕𝗢𝗧       \n\n_থাপ্পর👋 খা হালার পুত_😛👉 ${tag}` + `\n\nসরি, আমার ভুল হয়েছে মাফ করে দিন_🙁😸\n\n  BOT OWNER JOWEL ッ\n╰──────•◈•───────╯`,mentions: [{tag: tag,id: Object.keys(event.mentions)[0]}],attachment: fs.createReadStream(__dirname + "/cache/slp.gif")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/slp.gif"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/slp.gif")).on("close",() => callback());
               }
